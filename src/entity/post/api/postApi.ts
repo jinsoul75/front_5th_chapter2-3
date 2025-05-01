10f5308e3602d1feb61289bc0d05be2d08471e79
jinsoul75 <wlsthf75@gmail.com>
2025-05-01 23:53:01 +0900

    chore: 배포 관련 설정

--- src/entity/post/api/postApi.ts
+++ src/entity/post/api/postApi.ts
@@ -1,11 +1,11 @@
-import axios from "axios"
+import { axiosInstance } from "@/shared/api/axiosInstance"
 import { NewPost, Post } from "@/entity/post/types/postTypes"
 
 export const postsApi = {
   // 게시물 목록 가져오기
   fetchPosts: async (limit: number, skip: number, sortBy: string, sortOrder: string) => {
     try {
-      const { data } = await axios(`/api/posts`, { params: { limit, skip, sortBy, sortOrder } })
+      const { data } = await axiosInstance(`/posts`, { params: { limit, skip, sortBy, sortOrder } })
       return data
     } catch (error) {
       console.error("게시물 목록 가져오기 오류:", error)
@@ -16,7 +16,7 @@ export const postsApi = {
   // 게시물 검색
   searchPosts: async (query: string, limit: number, skip: number, sortBy: string, sortOrder: string) => {
     try {
-      const { data } = await axios("/api/posts/search", { params: { q: query, limit, skip, sortBy, sortOrder } })
+      const { data } = await axiosInstance("/posts/search", { params: { q: query, limit, skip, sortBy, sortOrder } })
       return data
     } catch (error) {
       console.error("게시물 검색 오류:", error)
@@ -27,7 +27,7 @@ export const postsApi = {
   // 태그별 게시물
   fetchPostsByTag: async (tag: string, limit: number, skip: number, sortBy: string, sortOrder: string) => {
     try {
-      const { data } = await axios(`/api/posts/tag/${tag}`, { params: { limit, skip, sortBy, sortOrder } })
+      const { data } = await axiosInstance(`/posts/tag/${tag}`, { params: { limit, skip, sortBy, sortOrder } })
       return data
     } catch (error) {
       console.error("태그별 게시물 가져오기 오류:", error)
@@ -38,7 +38,7 @@ export const postsApi = {
   // 게시물 추가
   addPost: async (newPost: NewPost) => {
     try {
-      const { data } = await axios.post("/api/posts/add", newPost)
+      const { data } = await axiosInstance.post("/posts/add", newPost)
       return data
     } catch (error) {
       console.error("게시물 추가 오류:", error)
@@ -49,7 +49,7 @@ export const postsApi = {
   // 게시물 수정
   updatePost: async (post: Post) => {
     try {
-      const { data } = await axios.put(`/api/posts/${post.id}`, post)
+      const { data } = await axiosInstance.put(`/posts/${post.id}`, post)
       return data
     } catch (error) {
       console.error("게시물 수정 오류:", error)
@@ -60,7 +60,7 @@ export const postsApi = {
   // 게시물 삭제
   deletePost: async (id: number) => {
     try {
-      const { data } = await axios.delete(`/api/posts/${id}`)
+      const { data } = await axiosInstance.delete(`/posts/${id}`)
       return data
     } catch (error) {
       console.error("게시물 삭제 오류:", error)
@@ -71,7 +71,7 @@ export const postsApi = {
   // 태그 가져오기
   fetchTags: async () => {
     try {
-      const { data } = await axios("/api/posts/tags")
+      const { data } = await axiosInstance("/posts/tags")
       return data
     } catch (error) {
       console.error("태그 가져오기 오류:", error)
